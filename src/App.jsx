0dbf9a22972c70fd06523e16f23d5b5750258bab
swaminaing <smnaing9.smn@gmail.com>
2025-03-11 22:28:49 +0630

    Tailwind set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
