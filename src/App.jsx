2eb5d9ee42845d0673f77b520bd57853a07791f9
swaminaing <smnaing9.smn@gmail.com>
2025-03-13 14:03:33 +0630

    create basic aside bar ui design and install swiper for carousel animation

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ function App() {
       <Navbar />
       <section>
         <div className="grid grid-cols-4">
-          <div className="col-span-1 border-r p-4">
+          <div
+            className="col-span-1 bg-white
+           border-r p-4"
+          >
             <Aside />
           </div>
           <div className="col-span-3">
--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,14 +1,66 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 function Aside() {
   return (
     <div>
-      <div className="bg-white">
-        <h3>Jane Doe</h3>
-        <span className="text-[12px]">Teacher</span>
+      <div className="relative mb-5">
+        <div className="bg-black opacity-1.5 absolute rounded-xl inset-0"></div>
+        <div className="relative z-10 p-4">
+          <h3 className="text-black">Jane Doe</h3>
+          <span className="text-[13px] text-[#626262]">Developer</span>
+        </div>
       </div>
 
-      <div className="button-group"></div>
+      <div className="relative mb-5">
+        <div className="bg-black opacity-1.5 absolute rounded-xl inset-0"></div>
+        <div className="relative z-10 p-4">
+          <p className="underline">Menu</p>
+          <ul className="text-[#626262] mt-2">
+            <li>
+              <a href="#" className="text-[13px] hover:text-black">
+                Dashboard +
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="relative">
+        <div className="bg-black opacity-1.5 absolute rounded-xl inset-0"></div>
+        <div className="relative z-10 p-4">
+          <p className="underline mb-3">Authors</p>
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            className="w-full"
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            modules={[Autoplay]}
+          >
+            <SwiperSlide>
+              <div className="p-3 bg-gray-800 rounded-lg text-white">
+                <h4>John Smith</h4>
+                <span className="text-[13px] text-gray-400">Writer</span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="p-3 bg-gray-800 rounded-lg text-white">
+                <h4>Emily Johnson</h4>
+                <span className="text-[13px] text-gray-400">Editor</span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="p-3 bg-gray-800 rounded-lg text-white">
+                <h4>Michael Brown</h4>
+                <span className="text-[13px] text-gray-400">Blogger</span>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 }
