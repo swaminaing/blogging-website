79443be33dd14e97b39c295bfa670cdbe29d9365
swaminaing <smnaing9.smn@gmail.com>
2025-03-13 21:30:32 +0630

    first step to react router routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
