29a6b4c615c64f6c861d8be604e7b2271169a411
swaminaing <smnaing9.smn@gmail.com>
2025-03-14 14:11:42 +0630

    implement login form logic

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navigation Bar/Navbar";
-// import "../Login/Login.css";
 
 const styles = {
   container: "w-full h-[90vh] flex justify-center items-center",
@@ -8,17 +7,42 @@ const styles = {
   formGroup: "w-full mb-5 relative group",
   formLabel: "text-[#626262] font-medium transition-colors duration-300",
   formInput:
-    "w-full block border-[#626262] border-b mt-3 focus:outline-none focus:ring-0",
+    "w-full text-sm block border-[#626262] border-b mt-3 focus:outline-none focus:ring-0",
   formButton:
     "w-full border-1 rounded-2xl py-2 mt-7 bg-amber-600 text-white hover:bg-amber-700 cursor-pointer transition-all",
 };
 
 function Login() {
+  const [loginedUser, setLoginedUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (loginedUser.username && loginedUser.password) {
+      localStorage.setItem("loginedUser", JSON.stringify(loginedUser));
+    } else {
+      alert("Please enter both username and password.");
+    }
+
+    setLoginedUser({
+      username: "",
+      password: "",
+    });
+  }
+
+  function handleChange(e) {
+    setLoginedUser((prevUser) => ({
+      ...prevUser,
+      [e.target.name]: e.target.value,
+    }));
+  }
   return (
     <div>
       <Navbar />
       <div className={styles.container}>
-        <form className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <h1 className="text-2xl font-bold mb-7">Login Form</h1>
 
           <div className={styles.formGroup}>
@@ -28,7 +52,14 @@ function Login() {
             >
               Username:
             </label>
-            <input type="text" id="username" className={styles.formInput} />
+            <input
+              name="username"
+              value={loginedUser.username}
+              type="text"
+              id="username"
+              className={styles.formInput}
+              onChange={handleChange}
+            />
           </div>
 
           <div className={styles.formGroup}>
@@ -38,10 +69,19 @@ function Login() {
             >
               Password:
             </label>
-            <input type="password" id="password" className={styles.formInput} />
+            <input
+              name="password"
+              value={loginedUser.password}
+              type="password"
+              id="password"
+              className={styles.formInput}
+              onChange={handleChange}
+            />
           </div>
 
-          <button className={styles.formButton}>Login</button>
+          <button type="submit" className={styles.formButton}>
+            Login
+          </button>
         </form>
       </div>
     </div>
